e1e4ad0b5a1df3bc17a257f9d133717534001165
Sarthak-Singh02 <sarthaksingh382495@gmail.com>
2022-11-17 08:56:20 +0530

    first commit

--- lib/activity/Tabs/TrendingTab.dart
+++ lib/activity/Tabs/TrendingTab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hear_me/Queries.dart';
+import 'package:hear_me/activity/Episodes.dart';
 
 class TrendingTab extends StatelessWidget {
   const TrendingTab({super.key});
@@ -45,23 +46,32 @@ class TrendingTab extends StatelessWidget {
                   itemCount: response.length,
                   itemBuilder: (context, index) => Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: GridTile(
-                        child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image.network(response[index]["imageUrl"],
-                          frameBuilder:
-                              (context, child, frame, wasSynchronouslyLoaded) {
-                        return child;
-                      }, loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) {
+                    child: InkWell(
+                      onTap: () {
+                         Navigator.push<void>(
+    context,
+    MaterialPageRoute<void>(
+      builder: (BuildContext context) => const Episodes(),
+    ),);
+                      },
+                      child: GridTile(
+                          child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.network(response[index]["imageUrl"],
+                            frameBuilder:
+                                (context, child, frame, wasSynchronouslyLoaded) {
                           return child;
-                        } else {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                      }),
-                    )),
+                        }, loadingBuilder: (context, child, loadingProgress) {
+                          if (loadingProgress == null) {
+                            return child;
+                          } else {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                        }),
+                      )),
+                    ),
                   ),
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 1,
